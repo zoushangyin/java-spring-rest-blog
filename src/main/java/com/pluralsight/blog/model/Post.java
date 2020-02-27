f3db8f0b6dc1451e47615871e5477d21a4c4d046
sarah <sarah-holderness@pluralsight.com>
2020-02-27 17:57:55 -0500

    Module 3 solution

--- src/main/java/com/pluralsight/blog/model/Post.java
+++ src/main/java/com/pluralsight/blog/model/Post.java
@@ -17,6 +17,10 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Version
+    private Long version;
+    @NotNull
+    @Size(min=4, max=100)
     private String title;
     @Column(length=1000000)
     @Lob
