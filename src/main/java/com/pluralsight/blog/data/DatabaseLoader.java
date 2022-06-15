ec223062a396d36aba06e5e38ce1668092f2c2b1
shangyin.zou <zoushangyin@gmail.com>
2022-06-15 16:35:52 +0900

    module 1

--- src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
+++ src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
@@ -8,12 +8,13 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
 @Component
 public class DatabaseLoader implements ApplicationRunner {
+
+    private final PostRepository postRepository;
     private final String[] templates = {
             "Smart Home %s", "Mobile %s - For When You're On he Go", "The %s - Your New Favorite Accessory"};
     private final String[] gadgets = {
@@ -21,7 +22,9 @@ public class DatabaseLoader implements ApplicationRunner {
     public List<Post> randomPosts = new ArrayList<>();
     public List<Author> authors = new ArrayList<>();
 
-    public DatabaseLoader() {
+    @Autowired
+    public DatabaseLoader(PostRepository postRepository) {
+        this.postRepository = postRepository;
     }
 
     @Override
@@ -34,5 +37,6 @@ public class DatabaseLoader implements ApplicationRunner {
             Post post = new Post(title, "Lorem ipsum dolor sit amet, consectetur adipiscing elit… ");
             randomPosts.add(post);
         });
+        postRepository.saveAll(randomPosts);
     }
 }
--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -3,27 +3,35 @@ package com.pluralsight.blog.data;
 import com.pluralsight.blog.model.Author;
 import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Component
-public class PostRepository {
-    public List<Post> findAll() {
-        return new ArrayList<>();
-    }
 
-    public List<Post> saveAll(List<Post> posts) {
-        return new ArrayList<>();
-    }
+public interface PostRepository extends JpaRepository<Post, Long> {
 
-    public Post save(Post post) {
-        return null;
-    }
+    @RestResource(rel = "contains-title", path = "containsTitle")
+    public List<Post> findByTitleContaining(String title);
 
-    public Optional<Post> findById(Long id) {
-        return null;
-    }
 }
+//@Component
+//public class PostRepository {
+//    public List<Post> findAll() {
+//        return new ArrayList<>();
+//    }
+//
+//    public List<Post> saveAll(List<Post> posts) {
+//        return new ArrayList<>();
+//    }
+//
+//    public Post save(Post post) {
+//        return null;
+//    }
+//
+//    public Optional<Post> findById(Long id) {
+//        return null;
+//    }
+//}
